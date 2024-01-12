a87cc9faec728abd47f149423987da76ab59fa0d
Nepthaly Soberanis <nsoberanis05@gmail.com>
2024-01-12 11:59:09 -0600

    deleted scope

--- routes/auth.js
+++ routes/auth.js
@@ -56,7 +56,6 @@ router.get("/discord/callback", async (req, res) => {
       grant_type: "authorization_code",
       code,
       redirect_uri: process.env.DISCORD_REDIRECT_URI,
-      scope: "identify",
     });
 
     const headers = {
